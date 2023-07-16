6119126b010a549adca7d48de3f19bb789fc6eb4
Kaushik Biswas <kbutsho@gmail.com>
2023-07-16 14:02:57 +0600

    private route added

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -7,12 +7,9 @@ interface IProps {
 }
 
 export default function PrivateRoute({ children }: IProps) {
-    const { user, isLoading } = useAppSelector((state) => state.user);
+    const { email } = useAppSelector((state) => state.user);
     const { pathname } = useLocation();
-    if (isLoading) {
-        return <p>Loading...</p>;
-    }
-    if (!user.email && !isLoading) {
+    if (!email) {
         return <Navigate to="/login" state={{ path: pathname }} />;
     }
     return children;

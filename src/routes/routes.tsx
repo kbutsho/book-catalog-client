ae723d9f036f9d21cfe5064b1a6d104a31101403
Kaushik Biswas <kbutsho@gmail.com>
2023-07-16 19:54:46 +0600

    fix error

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,7 +3,6 @@ import App from "../App";
 import Home from "../pages/Home";
 import BookDetails from "../pages/BookDetails";
 import PrivateRoute from "./PrivateRoute";
-import Checkout from "../pages/Checkout";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import NotFound from "../pages/NotFound";

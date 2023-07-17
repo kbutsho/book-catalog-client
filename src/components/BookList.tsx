d28265a68dbffeaf8ac8b752dcbce4640c44a02b
Kaushik Biswas <kbutsho@gmail.com>
2023-07-17 16:17:11 +0600

    added wish and reading list

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react'
 import { useDeleteBookMutation } from "../redux/features/books/bookApi";
-import { useAppSelector } from "../redux/hook";
+import { useAppDispatch, useAppSelector } from "../redux/hook";
 import { IBook } from "../types/globalTypes";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Modal, ModalBody, ModalFooter } from "reactstrap";
 import { useNavigate } from 'react-router-dom';
+import { addToWishList } from '../redux/features/wishList/wishListSlice';
+import { addToReadingList } from '../redux/features/readingList/readingListSLice';
 interface RefetchFunction {
     (): void;
 }
@@ -17,6 +19,7 @@ interface TableRowProps {
 const BookList: React.FC<TableRowProps> = ({ book, index, refetch }) => {
     const { userId } = useAppSelector((state) => state.user)
     const navigate = useNavigate()
+    const dispatch = useAppDispatch();
     // delete book 
     const [modal, setModal] = useState(false);
     const toggleModal = () => {
@@ -54,6 +57,23 @@ const BookList: React.FC<TableRowProps> = ({ book, index, refetch }) => {
             toast.error('login first!')
         }
     }
+    // add to wishlist
+    const handelAddToWishList = (book: IBook) => {
+        if (userId) {
+            dispatch(addToWishList(book));
+        } else {
+            toast.error('login first for add to wishlist!')
+        }
+    }
+    // add to reading list
+    const handelAddToReadingList = (book: IBook) => {
+        if (userId) {
+            dispatch(addToReadingList(book));
+        } else {
+            toast.error('login first for add to reading list!')
+        }
+    }
+
     return (
         <tr key={book._id}>
             <td>{index + 1}</td>
@@ -62,8 +82,8 @@ const BookList: React.FC<TableRowProps> = ({ book, index, refetch }) => {
             <td>{book.publicationDate.split('T')[0].split('-').reverse().join('/')}</td>
             <td>{book.genre}</td>
             <td className="d-flex">
-                <button className='btn btn-warning btn-sm fw-bold me-1'>Reading</button>
-                <button className='btn btn-info btn-sm fw-bold me-1'>Wishlist </button>
+                <button onClick={() => handelAddToReadingList(book)} className='btn btn-warning btn-sm fw-bold me-1'>Reading</button>
+                <button onClick={() => handelAddToWishList(book)} className='btn btn-info btn-sm fw-bold me-1'>Wishlist </button>
                 <button onClick={() => handelDetails(book._id)} className="btn btn-primary btn-sm fw-bold">Details</button>
                 <button onClick={() => handelUpdateBook(book._id)} className="btn btn-success btn-sm fw-bold mx-1">Update</button>
                 <button onClick={toggleModal} className="btn btn-danger btn-sm fw-bold">Delete</button>
